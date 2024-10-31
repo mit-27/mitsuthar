4177bdd8ff77a9f6ffc02dc85ebb338f65470acc
mit-27 <meetsuthar64@gmail.com>
2024-10-30 23:47:35 -0600

    🎨 Add width of layout

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component }: PageProps) {
         <title>mitsuthar</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body class="bg-[#090B16]">
+      <body class="bg-[#090B16] max-w-3xl mx-auto">
         <Component />
       </body>
     </html>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div class="container mx-auto flex flex-col items-center justify-center border-blue-600 border max-w-">
+    <div className="min-h-screen border-red-600 border">
       <h1 class="text-4xl font-bold text-white">Hello World!</h1>
       <p class="text-xl text-white">
         This is a fresh project generated with{" "}
